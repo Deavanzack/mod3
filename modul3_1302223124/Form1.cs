91eca8b6c874ba196d4a915fe8a6238a1ab34912
PRAKTIKAN <PRAKTIKAN@PC-10-0712>
2024-03-07 10:35:39 +0700

    Add project files.

---
+++ modul3_1302223124/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modul3_1302223124
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text = "0";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text = "8";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            label1.Text = "12";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = "6";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "2";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "1";
+        }
+    }
+}
